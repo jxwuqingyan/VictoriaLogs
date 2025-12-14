35c2d1282d34949a3f3e4f02ea3a14e9d7117559
Aliaksandr Valialkin <valyala@victoriametrics.com>
2025-12-14 23:05:34 +0100

    app/vlinsert/opentelemetry: update the BenchmarkPushProtobufRequest
    - Rename BenchmarkParseProtobufRequest to BenchmarkPushProtobufRequest in order to reflect
      the function name to benchmark - pushProtobufRequest().
    - Add missing value types and protobuf fields into the protobuf message to parse.
    - Prepare the request body only once and parse it from parallel goroutines.
    - Measure the bytes parsed per second instead of log entries parsed per second.

--- app/vlinsert/opentelemetry/opentelemetry_timing_test.go
+++ app/vlinsert/opentelemetry/opentelemetry_timing_test.go
@@ -7,24 +7,25 @@ import (
 	"github.com/VictoriaMetrics/VictoriaLogs/app/vlinsert/insertutil"
 )
 
-func BenchmarkParseProtobufRequest(b *testing.B) {
+func BenchmarkPushProtobufRequest(b *testing.B) {
 	for _, scopes := range []int{1, 2} {
 		for _, rows := range []int{1, 10, 100, 1000} {
 			for _, attributes := range []int{5, 10} {
 				b.Run(fmt.Sprintf("scopes_%d/rows_%d/attributes_%d", scopes, rows, attributes), func(b *testing.B) {
-					benchmarkParseProtobufRequest(b, scopes, rows, attributes)
+					benchmarkPushProtobufRequest(b, scopes, rows, attributes)
 				})
 			}
 		}
 	}
 }
 
-func benchmarkParseProtobufRequest(b *testing.B, streams, rows, labels int) {
+func benchmarkPushProtobufRequest(b *testing.B, streams, rows, labels int) {
+	body := getProtobufBody(streams, rows, labels)
+
 	blp := &insertutil.BenchmarkLogMessageProcessor{}
 	b.ReportAllocs()
-	b.SetBytes(int64(streams * rows))
+	b.SetBytes(int64(len(body)))
 	b.RunParallel(func(pb *testing.PB) {
-		body := getProtobufBody(streams, rows, labels)
 		for pb.Next() {
 			if err := pushProtobufRequest(body, blp, nil, false); err != nil {
 				panic(fmt.Errorf("unexpected error: %w", err))
@@ -34,30 +35,65 @@ func benchmarkParseProtobufRequest(b *testing.B, streams, rows, labels int) {
 }
 
 func getProtobufBody(scopesCount, rowsCount, attributesCount int) []byte {
-	msg := "12345678910"
-
 	attrValues := []*anyValue{
 		{StringValue: ptrTo("string-attribute")},
+		{BoolValue: ptrTo(true)},
 		{IntValue: ptrTo[int64](12345)},
 		{DoubleValue: ptrTo(3.14)},
+		{
+			ArrayValue: &arrayValue{
+				Values: []*anyValue{
+					{StringValue: ptrTo("abc")},
+				},
+			},
+		},
+		{
+			KeyValueList: &keyValueList{
+				Values: []*keyValue{
+					{
+						Key: "foobarbaz",
+						Value: &anyValue{
+							StringValue: ptrTo("xyzqwe"),
+						},
+					},
+				},
+			},
+		},
 	}
+
 	attrs := make([]*keyValue, attributesCount)
 	for j := 0; j < attributesCount; j++ {
 		attrs[j] = &keyValue{
 			Key:   fmt.Sprintf("key-%d", j),
-			Value: attrValues[j%3],
+			Value: attrValues[j%len(attrValues)],
 		}
 	}
 	entries := make([]logRecord, rowsCount)
 	for j := 0; j < rowsCount; j++ {
 		entries[j] = logRecord{
-			TimeUnixNano: 12345678910, ObservedTimeUnixNano: 12345678910, Body: anyValue{StringValue: &msg},
+			TimeUnixNano: 12345678910,
+			ObservedTimeUnixNano: 12345678910,
+			Body: anyValue{
+				StringValue: ptrTo("12345678910"),
+			},
 		}
 	}
 	scopes := make([]scopeLogs, scopesCount)
 
 	for j := 0; j < scopesCount; j++ {
 		scopes[j] = scopeLogs{
+			Scope: &instrumentationScope{
+				Name: "abc",
+				Version: "v1.2.345",
+				Attributes: []*keyValue{
+					{
+						Key: "qwe",
+						Value: &anyValue{
+							StringValue: ptrTo("ierweo"),
+						},
+					},
+				},
+			},
 			LogRecords: entries,
 		}
 	}
