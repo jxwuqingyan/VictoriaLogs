jxwuqingyan/VictoriaLogs

commit 7f66493f5939de08b8af608b5ab2ed04b9c64c72

lib/logstorage: properly skip fields with more than max_values_per_field values at `facets` pipe

lib/logstorage/pipe_facets.go
@@ -63,7 +63,11 @@ func (pf *pipeFacets) splitToRemoteAndLocal(timestamp int64) (pipe, []pipe) {
 	pRemote := *pf
 	pRemote.limit = math.MaxUint64
 
-	psLocalStr := fmt.Sprintf("stats by (field_name, field_value) sum(hits) as hits | sort by (hits desc) limit %d partition by (field_name) | sort by (field_name, hits desc)", pf.limit)
+	psLocalStr := fmt.Sprintf(`stats by (field_name, field_value) sum(hits) as hits
+	        | total_stats by (field_name) count() as field_values_count
+		| filter field_values_count:<=%d
+		| sort by (hits desc) limit %d partition by (field_name)
+		| sort by (field_name, hits desc)`, pf.maxValuesPerField, pf.limit)
 	psLocal := mustParsePipes(psLocalStr, timestamp)
 
 	return &pRemote, psLocal
@@ -351,6 +355,7 @@ func (pfp *pipeFacetsProcessor) flush() error {
 		return nil
 	}
 
+	ignoreFields := make(map[string]bool)
 	hmasByFieldName := make(map[string][]*hitsMapAdaptive)
 	rowsTotal := uint64(0)
 	for _, shard := range shards {
@@ -358,7 +363,12 @@ func (pfp *pipeFacetsProcessor) flush() error {
 			return nil
 		}
 		for fieldName, fhs := range shard.m {
+			if ignoreFields[fieldName] {
+				continue
+			}
 			if fhs.mustIgnore {
+				ignoreFields[fieldName] = true
+				delete(hmasByFieldName, fieldName)
 				continue
 			}
 			hmasByFieldName[fieldName] = append(hmasByFieldName[fieldName], &fhs.m)

lib/logstorage/net_query_runner_test.go
@@ -47,7 +47,7 @@ func TestSplitQueryToRemoteAndLocal(t *testing.T) {
 	f(`foo | drop_empty_fields`, `foo | drop_empty_fields`, ``)
 	f(`foo | extract "foo<bar>baz"`, `foo | extract "foo<bar>baz"`, ``)
 	f(`foo | extract_regexp "foo(?P<ip>[^;]+)"`, `foo | extract_regexp "foo(?P<ip>[^;]+)"`, ``)
-	f(`foo | facets`, `foo | facets 18446744073709551615 | fields field_name, field_value, hits`, `stats by (field_name, field_value) sum(hits) as hits | sort by (hits desc) partition by (field_name) limit 10 | sort by (field_name, hits desc)`)
+	f(`foo | facets`, `foo | facets 18446744073709551615 | fields field_name, field_value, hits`, `stats by (field_name, field_value) sum(hits) as hits | total_stats by (field_name) count(*) as field_values_count | filter field_values_count:<=1000 | sort by (hits desc) partition by (field_name) limit 10 | sort by (field_name, hits desc)`)
 	f(`foo | field_names`, `foo | field_names | fields hits, name`, `stats by (name) sum(hits) as hits`)
 	f(`foo | field_values x`, `foo | field_values x | fields hits, x`, `field_values_local x`)
 	f(`foo | fields x, y`, `foo | fields x, y`, ``)

lib/logstorage/pipe_facets_test.go
@@ -68,6 +68,28 @@ func TestPipeFacets(t *testing.T) {
 		},
 	})
 
+	f("facets max_values_per_field 2", [][]Field{
+		{
+			{"a", `2`},
+			{"b", `asd54`},
+		},
+		{
+			{"a", "2"},
+			{"b", "3"},
+		},
+		{
+			{"a", `2`},
+			{"b", `54`},
+			{"c", "d"},
+		},
+	}, [][]Field{
+		{
+			{"field_name", "c"},
+			{"field_value", "d"},
+			{"hits", "1"},
+		},
+	})
+
 	f("facets 1 keep_const_fields", [][]Field{
 		{
 			{"a", `2`},

commit 50af714e8b7bb14f8493e973afdab789444fc17a

lib/logstorage: drop the auxiliary field_values_count field created at the facets pipe handler executed at vlselect after receiving facets from vlstorage nodes
The /select/logsql/facets endpoint doesn't expect the field_values_count field,
so it panics with the `expecting 3 columns; got 4 columns` message at vlselect in cluster mode.

Updates https://github.com/VictoriaMetrics/VictoriaLogs/issues/940
Updates https://github.com/VictoriaMetrics/VictoriaLogs/pull/942

The bug has been introduced in https://github.com/VictoriaMetrics/VictoriaLogs/commit/7f66493f5939de08b8af608b5ab2ed04b9c64c72
in VictoriaLogs v1.35.0.

lib/logstorage/pipe_facets.go
@@ -66,6 +66,7 @@ func (pf *pipeFacets) splitToRemoteAndLocal(timestamp int64) (pipe, []pipe) {
 	psLocalStr := fmt.Sprintf(`stats by (field_name, field_value) sum(hits) as hits
 	        | total_stats by (field_name) count() as field_values_count
 		| filter field_values_count:<=%d
+		| delete field_values_count
 		| sort by (hits desc) limit %d partition by (field_name)
 		| sort by (field_name, hits desc)`, pf.maxValuesPerField, pf.limit)
 	psLocal := mustParsePipes(psLocalStr, timestamp)

lib/logstorage/net_query_runner_test.go
@@ -47,7 +47,7 @@ func TestSplitQueryToRemoteAndLocal(t *testing.T) {
 	f(`foo | drop_empty_fields`, `foo | drop_empty_fields`, ``)
 	f(`foo | extract "foo<bar>baz"`, `foo | extract "foo<bar>baz"`, ``)
 	f(`foo | extract_regexp "foo(?P<ip>[^;]+)"`, `foo | extract_regexp "foo(?P<ip>[^;]+)"`, ``)
-	f(`foo | facets`, `foo | facets 18446744073709551615 | fields field_name, field_value, hits`, `stats by (field_name, field_value) sum(hits) as hits | total_stats by (field_name) count(*) as field_values_count | filter field_values_count:<=1000 | sort by (hits desc) partition by (field_name) limit 10 | sort by (field_name, hits desc)`)
+	f(`foo | facets`, `foo | facets 18446744073709551615 | fields field_name, field_value, hits`, `stats by (field_name, field_value) sum(hits) as hits | total_stats by (field_name) count(*) as field_values_count | filter field_values_count:<=1000 | delete field_values_count | sort by (hits desc) partition by (field_name) limit 10 | sort by (field_name, hits desc)`)
 	f(`foo | field_names`, `foo | field_names | fields hits, name`, `stats by (name) sum(hits) as hits`)
 	f(`foo | field_values x`, `foo | field_values x | fields hits, x`, `field_values_local x`)
 	f(`foo | fields x, y`, `foo | fields x, y`, ``)